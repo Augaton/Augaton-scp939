21b43759ff1ab1953334f80202cd2b8303e2ce8a
Augaton <95074536+Augaton@users.noreply.github.com>
2025-08-22 08:12:54 +0000

    déplacement V2

---
+++ lua/guthscp/modules/scp939/client.lua
@@ -0,0 +1,28 @@
+local scp939 = guthscp.modules.scp939
+local config939 = guthscp.configs.scp939
+scp939 = scp939 or {}
+
+net.Receive('SCP939HUDOFF', function()
+    hook.Remove("Think", "SCP939NoDraw")
+    hook.Remove("RenderScreenspaceEffects", "PostProcess939")
+    hook.Remove("PreDrawOpaqueRenderables", "HidePlayersForSCP939")
+    hook.Remove("PostDrawTranslucentRenderables", "DrawPlayersSCP939")
+
+    for _, v in ipairs(player.GetAll()) do
+        if v ~= LocalPlayer() then
+            if v:GetMaterial() ~= "" then
+                v:SetMaterial("")
+            end
+
+            if IsValid(v:GetActiveWeapon()) then
+                v:GetActiveWeapon():SetNoDraw(false)
+            end
+        end
+
+        v:SetNoDraw(false)
+    end
+
+    render.SetBlend(1)  -- Juste au cas où, reset du blend
+    render.MaterialOverride(nil)
+    render.SetColorModulation(1, 1, 1)
+end)
